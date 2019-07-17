ad421bfbf776c63ee7ff6f5f6bb82ba9b56e3043
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-17 16:23:43 +0000

    Deploy italoiz/bossabox-frontend-challenge to github.com/italoiz/bossabox-frontend-challenge.git:gh-pages

---
+++ reference/precache-manifest.42a49735af6ce5a5f4c4eef2bcafc4a6.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "202f3ff288403bd86115e866d8b35825",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.405751e4de17707448be.bundle.js"
+  },
+  {
+    "url": "runtime~main.405751e4de17707448be.bundle.js"
+  },
+  {
+    "url": "vendors~main.405751e4de17707448be.bundle.js"
+  },
+  {
+    "revision": "de97f8096b30d6753f9e",
+    "url": "vendors~main.css"
+  }
+]);
